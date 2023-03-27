a60047f7c3ec8742ac6d1387b108357a0f65bf77
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-03-27 15:58:44 +0200

    modified JS file

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ const add = document.querySelector('[data-key="add"]');
 const subtractHandler = () => {
         const newValue = parseInt(number.value) - 1;
         number.value = newValue;
+        
+        if (add.disabled ===true) {
+                add.disabled = false
+        };
+
+        if  (newValue <= MIN_NUMBER) {
+                 subtract.disabled  = true
+        };
 
 };
 
 const addHandler = () => {
         const newValue = parseInt(number.value) + 1;
         number.value = newValue;
+
+        if (subtract.disabled ===true) {
+                subtract.disabled = false
+        };
+
+        if  (newValue >= MAX_NUMBER) {
+                add.disabled = true
+        };
 
 };
 
