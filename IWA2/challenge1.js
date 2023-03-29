b7844e925b176c07c294c6e9268f74e9f26dd0ef
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-03-29 09:22:10 +0200

    modified challenge1.js

--- IWA2/challenge1.js
+++ IWA2/challenge1.js
@@ -1,3 +1,5 @@
+//challenge1
+
 // This is the firstname of the user
 
 const user = 'John'
@@ -7,3 +9,29 @@ const user = 'John'
 const surname = 'Smith'
 
 console.log(user, surname);
+
+//challenge2 
+
+/* (c) ACME Inc. 2010 */
+
+/** 
+  This is the date that a user created their account 
+ */
+  const date = '10/07/2014'
+
+  console.log(date)
+
+  //challenge3
+
+  /* 
+It is important to show the following message in order to:
+*- to assure users
+*- to scare hackers
+*- to impress investors
+*/
+
+console.warn("Security scan starting");
+
+/* It is important to let user know when they can close the page */
+
+console.info("Please wait for scan to complete before closing the browser.")
