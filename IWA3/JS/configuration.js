45f3aa76f4ab32f4ddb413ca24b731e7a80df94b
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-03-29 13:54:31 +0200

    modified index.html config.js and script.js

---
+++ IWA3/JS/configuration.js
@@ -0,0 +1,5 @@
+
+ export const company = 'ACME Inc.'
+ export const year = 2022
+
+ 
---
+++ IWA3/JS/scripts.js
@@ -0,0 +1,7 @@
+import {company} from './configuration.js'
+import {year} from './configuration.js'
+
+const message = '© ' + company + year ;
+document.getElementById("footer").innerText = message
+
+
