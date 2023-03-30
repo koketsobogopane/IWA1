8545ae942e94af96e3ff656845faeb7ffdc9144c
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-03-30 17:27:10 +0200

    modified javascript.js

--- IWA5/script.js
+++ IWA5/script.js
@@ -2,7 +2,7 @@ FREE_WARNING = 'Free shipping only applies to single customer orders'
 BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
 NONE_SELECTED = 0
 const customers = 1
-const clocation = "NK"
+const clocation = "RSA"
 let currency = null
 let shipping = null
 
@@ -21,7 +21,7 @@ if (clocation === "RSA" || clocation === "NAM") {
      shipping = 800
  }
 
-let shoes = 300 * 3
+let shoes = 300 * 1
 let toys = 100 * 5
 let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2
@@ -32,28 +32,28 @@ let total = shoes + toys + shirts + batteries + pens +shirts
 
 if ( total > 1000 && (clocation === "RSA" || clocation === "NAM") ) {
 		shipping = 0
-        console.log ("Shipping is free!!")
+        if (shipping === 0 && customers !== 1) {
+            console.log(FREE_WARNING)
+
+            if (clocation === 'RSA') {
+                shipping = 400
+            }
+            else if (clocation === "NAM"){
+                shipping = 600
+            }
+            else {
+                shipping = 800
+            }
+            
+        
 	}
+    else {
+        console.log ("Shipping is free!!")
+    }
+}
 
-if (shipping === 0 && customers !== 1) {
-     console.log(FREE_WARNING)
-     }
 
- clocation === 'NK' ? console.log(BANNED_WARNING) : console.log('price',currency, shoes + toys + batteries + pens +shirts)
-// customers = '1'
-// const location = 'RSA'
-// currency = null
+ clocation === 'NK' ? console.log(BANNED_WARNING) : console.log('price',currency, shoes + toys + batteries + pens + shirts + shipping)
 
-// shipping = null
-// currency = $
-
-// if ( total > 1000 && (clocation === "RSA" || clocation === "NAM") ) {
-// 	if (location = "NAM" && customers < 2) {
-// 			if (location === "RSA")
-// 		    shipping = 0 || calcShipping
-//             console.log ("FREE SHIPPING")
-//             console.log ("yes")
-// 		}
-// 	}
 
 
