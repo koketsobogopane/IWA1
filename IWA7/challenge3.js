03d6e8f52df9cfd818dfa8efaa8ba1eb1cef3085
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-04-06 14:16:34 +0200

    modified js files

--- IWA7/challenge3.js
+++ IWA7/challenge3.js
@@ -10,10 +10,11 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = `${ parseFloat((-1 * leoBalance) + (-1 * sarahBalance)).toFixed(2) }`
+const owed = `${ parseFloat((-1 * leoBalance) + (-1 * sarahBalance)).toFixed(2)}`
+const totalOwed = owed
 const leo = `${leoName} ${leoSurname.trim()} (Owed:R ${parseFloat(-1 * leoBalance).toFixed(2)})`
 const sarah = `${sarahName.trim()} ${sarahSurname} (Owed:R ${parseFloat(-1 * sarahBalance).toFixed(2)})`
 const total = "Total amount owed: "
-const result = `${leo}\n${sarah}\n\n${divider}\n\t${total}${owed}\n${divider}`
+const result = `${leo}\n${sarah}\n\n${divider}\n\t${total}${totalOwed}\n${divider}`
 
 console.log(result)
