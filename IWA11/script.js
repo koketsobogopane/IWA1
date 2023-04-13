b673093048efb9a0ea799ce5ab671c3133963ae5
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-04-13 13:08:30 +0200

    modified scripts.js

--- IWA11/script.js
+++ IWA11/script.js
@@ -1,40 +1,38 @@
 // script.js
 
-const root1 = document.querySelector('[data-key = order1] ')
 
-const biscuits1 = root1.dataset.biscuits
-const donut1 = root1.dataset.donut
-const pancake1 = root1.dataset.pancake
-const status1 = root1.dataset.status
-    //  '1-donuts': document.querySelector('donuts'),
-    //  '1-pancakes': document.querySelector('pancakes'),
-    //  '1-status': document.querySelector('status'),}}
+const root1 = {
+    biscuit1 : document.querySelector('[ data-biscuits="10"]'),
+    donut1 : document.querySelector('[data-donuts="13" ]'),
+    pancake1 : document.querySelector('[data-pancakes="0"]'),
+    status1 : document.querySelector('[data-delivered= "false"]'),
+}    
 
-// const root2 = {
-//     // document(order2)
-//  '2-biscuits': document.querySelector('biscuits'),
-//  '2-donuts': document.querySelector('donuts'),
-//  '2-pancakes': document.querySelector('pancakes'),
-//  '2-status': document.querySelector('status'),}
+const root2 = {
+    biscuit2 : document.querySelector('[ data-biscuits="5"]'),
+    donut2 : document.querySelector('[data-donuts="0" ]'),
+    pancake2 : document.querySelector('[data-pancakes="2"]'),
+    status2 : document.querySelector('[data-delivered= "false"]'),
+}    
 
-// const root3 = {
-//     // document(order3),
-//  '3-biscuits': document.querySelector(biscuits),
-//  '3-donuts': document.querySelector(donuts),
-//  '3-pancakes': document.querySelector(pancakes),
-//  '3-status': document.querySelector(status)}
+const root3 = {
+    biscuit3 : document.querySelector('[ data-biscuits="12"]'),
+    donut3 : document.querySelector('[data-donuts="11" ]'),
+    pancake3 : document.querySelector('[data-pancakes="15"]'),
+    status3 : document.querySelector('[data-delivered= "true"]'),
+}    
 
-document.querySelector("div.biscuits dd.count").innerHTML = biscuits1,
-document.querySelector("div.donuts dd.count").innerHTML = donut1,
-document.querySelector("div.pancakes dd.count").innerHTML = pancake1
-document.querySelector("div.status dd.count").innerHTML = status1 ? Delivered : Pending
+document.querySelector('dl[data-key= "order1"] div.biscuits dd.count').innerHTML = root1.biscuit1.dataset.biscuits
+document.querySelector('dl[data-key= "order1"] div.donuts dd.count').innerHTML = root1.donut1.dataset.donuts
+document.querySelector('dl[data-key= "order1"] div.pancakes dd.count').innerHTML = root1.pancake1.dataset.pancakes
+document.querySelector('dl[data-key= "order1"] div.status dd').innerHTML = root1.status1.dataset.delivered === "false"? "pending" : "Delivered"
 
-// '2-biscuits'= 2-root.biscuits,
-// '2-donuts' = 2-root.donuts,
-// '2-pancakes' = 2-root.pancakes,
-// '2-status' = 2-root.status ? Delivered : Pending
+document.querySelector('dl[data-key= "order2"] div.biscuits dd.count').innerHTML = root2.biscuit2.dataset.biscuits
+document.querySelector('dl[data-key= "order2"] div.donuts dd.count').innerHTML = root2.donut2.dataset.donuts
+document.querySelector('dl[data-key= "order2"] div.pancakes dd.count').innerHTML = root2.pancake2.dataset.pancakes
+document.querySelector('dl[data-key= "order2"] div.status dd').innerHTML = root2.status2.dataset.delivered === "false"? "pending" : "Delivered"
 
-// '3-biscuits' = 3-root.biscuits,
-// '3-donuts' = 3-root.donuts,
-// '3-pancakes' = 3-root.pancakes,
-// '3-status' = 3-root.status ? Delivered : Pending 
+ document.querySelector('dl[data-key= "order3"] div.biscuits dd.count').innerHTML = root3.biscuit3.dataset.biscuits
+ document.querySelector('dl[data-key= "order3"] div.donuts dd.count').innerHTML = root3.donut3.dataset.donuts
+ document.querySelector('dl[data-key= "order3"] div.pancakes dd.count').innerHTML = root3.pancake3.dataset.pancakes
+document.querySelector('dl[data-key= "order3"] div.status dd').innerHTML = root3.status3.dataset.delivered === "false"? "pending" : "Delivered"
